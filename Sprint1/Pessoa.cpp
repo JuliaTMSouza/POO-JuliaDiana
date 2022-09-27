c28f5af03e187e4cc5dfbb239dcab1316bc93369
JuliaTMSouza <jutarchi00@gmail.com>
2022-09-27 16:13:29 -0300

    27/09-ValidarCNPJ

--- Sprint1/Pessoa.cpp
+++ Sprint1/Pessoa.cpp
@@ -24,6 +24,57 @@ bool Pessoa::ValidarCPF_CNPJ(string CPF_CNPJ, bool Tipo) {
 
         return true;
     }
+
+    string cnpj = CPF_CNPJ.replace(/[^\d]+/g,'');
+
+    if(cnpj == '') return false;
+     
+    if (cnpj.length != 14) return false;
+
+    if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999") {
+            return false;
+    }
+
+    int tamanho = cnpj.length - 2
+    string numeros = cnpj.substring(0,tamanho);
+    string digitos = cnpj.substring(tamanho);
+    int soma = 0;
+    int pos = tamanho - 7;
+
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2) 
+            pos = 9;
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0,tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+          return false;
+           
+    return true;
+ 
 }
 
 string Pessoa::GetCPF_CNPJ() {
