0d0972337b3c15775a6765d118fce9fa5ab90d31
DianaPereira <dianarpereira2000@gmail.com>
2022-11-24 07:43:35 -0300

    Apenas ajeitei a main

--- main.cpp
+++ main.cpp
@@ -92,27 +92,26 @@ int main()
 
 
 
-//     cout << ">>>> SAIDA" << endl;
 
-//     Date aaa(2022, 9, 22);
-//     cout << aaa.getData() << endl;
+/*     Date aaa(2022, 9, 22);
+     cout << aaa.getData() << endl;
 
-//     Categoria Cozinha;
-//     Cozinha.SetNome("Cozinha");
-//     cout << Cozinha.GetNome() << endl;
+     Categoria Cozinha;
+     Cozinha.SetNome("Cozinha");
+     cout << Cozinha.GetNome() << endl;
 
-//     Valor preco;
-//     preco.SetData(Date(2022, 6, 24));
-//     preco.SetValor(25.99);
-//     cout << preco.GetValor() << endl; 
-//     cout << preco.GetData().getData() << endl;
+     Valor preco;
+     preco.SetData(Date(2022, 6, 24));
+     preco.SetValor(25.99);
+     cout << preco.GetValor() << endl; 
+     cout << preco.GetData().getData() << endl;
 
     Lote vamove();
     Lote novoLote(10, Date(2022, 9, 23), 1, 0);
     cout << novoLote.GetDataProducao().getData() << " " << novoLote.GetNumeroLote() << " " << novoLote.GetQuantidade() << endl;
 
-//     MateriaPrima mds;
-//     cout << ">> " << mds.GetLoteAtual() << endl;
+     MateriaPrima mds;
+     cout << ">> " << mds.GetLoteAtual() << endl;
 
     mds.SetEstoqueMinimo("20 m2");
     mds.SetEstoqueAtual(30);
@@ -120,25 +119,25 @@ int main()
     mds.SetLotes(45, aaa, 0);
     mds.SetNome("madeira");
 
-//     cout << mds.GetEstoqueAtual() << " ";
-//     cout << mds.GetEstoqueMinimo() << " ";
-//     cout << mds.GetLoteAtual() << " ";
-//     cout << mds.GetLotes().begin()->GetNumeroLote() << " ";
-//     cout << mds.GetNome() << " ";
-//     cout << mds.GetMedida() << " ";
-//     cout << "." << mds.GetUnidadeMedida() << ",\n" << endl;
+     cout << mds.GetEstoqueAtual() << " ";
+     cout << mds.GetEstoqueMinimo() << " ";
+     cout << mds.GetLoteAtual() << " ";
+     cout << mds.GetLotes().begin()->GetNumeroLote() << " ";
+     cout << mds.GetNome() << " ";
+     cout << mds.GetMedida() << " ";
+     cout << "." << mds.GetUnidadeMedida() << ",\n" << endl;
 
 
     Fornecedor chefe; //"020.247.096-21"
     chefe.SetCPF_CNPJ("02024709621", true);
     chefe.SetEmail("dianarpereira2000@gmail.com");
-   // chefe.SetMateriasPrima(mds.GetLotes());
+    //chefe.SetMateriasPrima(mds.GetLotes());
     chefe.SetNome("Robson");
-   // chefe.SetPrecoMateriais
+    //chefe.SetPrecoMateriais
 
-//     cout << chefe.GetCPF_CNPJ() << " ";
-//     cout << chefe.GetEmail() << " ";
-//     cout << chefe.GetNome() << "\n\n\n";
+     cout << chefe.GetCPF_CNPJ() << " ";
+     cout << chefe.GetEmail() << " ";
+     cout << chefe.GetNome() << "\n\n\n";
 
     Produto novoProduto(0); 
     cout << novoProduto.GetCodigoAtual() << "|" << novoProduto.GetLoteAtual() << "\n";
@@ -178,49 +177,49 @@ int main()
     cout << novoProduto2.GetLote().GetNumeroLote() << " ";
     cout << novoProduto2.GetValor().GetValor() << " ";
 
-//     cout << "\n\n O complexo:\n";
+     cout << "\n\n O complexo:\n";
 
-//     MateriaPrima mds1, mds2, mds3;
-//     cout << ">> " << mds.GetLoteAtual() << endl;
+     MateriaPrima mds1, mds2, mds3;
+     cout << ">> " << mds.GetLoteAtual() << endl;
 
     mds1.SetEstoqueMinimo("20 m2"); mds1.SetEstoqueAtual(30); mds1.SetMedida("20 m"); mds1.SetLotes(45, aaa, 0); mds1.SetNome("madeira");
     mds2.SetEstoqueMinimo("2 m"); mds2.SetEstoqueAtual(40); mds2.SetMedida("8 m"); mds2.SetLotes(45, aaa, 0); mds2.SetNome("parafuso");
     mds3.SetEstoqueMinimo("8 g"); mds3.SetEstoqueAtual(8); mds3.SetMedida("9 m"); mds3.SetLotes(45, aaa, 0); mds3.SetNome("perna");
 
-//     //cout <<  "\n Produto: \n";
-//     novoProduto.SetMateriasPrima(mds1);
-//     novoProduto.SetMateriasPrima(mds2);
-//     novoProduto.SetMateriasPrima(mds3);
+     //cout <<  "\n Produto: \n";
+     novoProduto.SetMateriasPrima(mds1);
+     novoProduto.SetMateriasPrima(mds2);
+     novoProduto.SetMateriasPrima(mds3);
 
-//     novoProduto.SolicitarNovoLote(1, Date(2023, 1, 24));
+     novoProduto.SolicitarNovoLote(1, Date(2023, 1, 24));
 
-//     //cout <<  "\n Produto2: \n";
-//     novoProduto2.SetMateriasPrima(mds1);
-//     novoProduto2.SetMateriasPrima(mds2);
-//     novoProduto2.SetMateriasPrima(mds3);
+     //cout <<  "\n Produto2: \n";
+     novoProduto2.SetMateriasPrima(mds1);
+     novoProduto2.SetMateriasPrima(mds2);
+     novoProduto2.SetMateriasPrima(mds3);
 
     novoProduto2.SolicitarNovoLote(5, Date(2023, 1, 24)); //funciona
 
     Orcamento novoOrcamento(&novoProduto, 7, Date(2023, 1, 24));
     cout << "orcamento: " << novoOrcamento.GetValorTotal() << endl;
 
-//     Boleto pagar1;
-//     pagar1.SetNome("Robson");
-//     pagar1.SetPrazo(60); //fazer o switch case
-//     pagar1.SetTipo("Boleto");
-//     pagar1.SetVencimento(Date(2023, 2, 24));
+     Boleto pagar1;
+     pagar1.SetNome("Robson");
+     pagar1.SetPrazo(60); //fazer o switch case
+     pagar1.SetTipo("Boleto");
+     pagar1.SetVencimento(Date(2023, 2, 24));
 
-//     cout << pagar1.GetNome() << " ";
-//     cout << pagar1.GetPrazo() << " ";
-//     cout << pagar1.GetTipo() << " ";
-//     cout << pagar1.GetVencimento().getData() << "\n";
+     cout << pagar1.GetNome() << " ";
+     cout << pagar1.GetPrazo() << " ";
+     cout << pagar1.GetTipo() << " ";
+     cout << pagar1.GetVencimento().getData() << "\n";
 
-//     Cartao pagar2;
-//     pagar2.SetNome("Robdaugther");
-//     pagar2.SetNumeroCartao("1234542395426541");
-//     pagar2.SetParcelas(6); //fazer o switch case
-//     pagar2.SetTipo("Cartao");
-//     pagar2.SetValidade(Date(2023, 2, 24));
+     Cartao pagar2;
+     pagar2.SetNome("Robdaugther");
+     pagar2.SetNumeroCartao("1234542395426541");
+     pagar2.SetParcelas(6); //fazer o switch case
+     pagar2.SetTipo("Cartao");
+     pagar2.SetValidade(Date(2023, 2, 24));
 
     cout << ">>> " << pagar2.GetNome() << " ";
     cout << pagar2.GetNumeroCartao() << " ";
@@ -305,7 +304,7 @@ int main()
     novoOnibus.SetPlaca(450);
     novoOnibus.SetRotas(novaRota);
     cout << novoOnibus.GetPlaca() << " " << novoOnibus.GetRotas().begin()->GetTurnos().GetHorarioPartida() << endl;
-
+*/
 
     return 0;
 
